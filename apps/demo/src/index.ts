1bf3aa11ccde852b9818f193d0e1fad909ab5eb3
muzzamilrevnix <muzzamil.revnix@gmail.com>
2025-09-02 19:17:59 +0500

    change packages names

--- apps/demo/src/index.ts
+++ apps/demo/src/index.ts
@@ -1,5 +1,5 @@
-import { capitalize, kebabCase } from "utilsss";
-import { sum, multiply } from "mathsss";
+import { capitalize, kebabCase } from "@muzzamil/utilsss";
+import { sum, multiply } from "@muzzamil/mathsss";
 
 console.log(capitalize("hello world")); // Hello world
 console.log(kebabCase("Hello World Example")); // hello-world-example
